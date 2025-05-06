7df8c61fcb56075a27ce00bb18e2752859fed936
kafierence <vannam34760@gmail.com>
2025-05-06 18:22:55 +0700

    [fix] update close Chat

--- packages/ai-service/src/agents/ai-agent.controller.ts
+++ packages/ai-service/src/agents/ai-agent.controller.ts
@@ -3,7 +3,6 @@ import {
   Post,
   Body,
   Get,
-  Query,
   HttpException,
   HttpStatus,
   Param,
@@ -11,23 +10,20 @@ import {
   Put,
 } from '@nestjs/common';
 import { AiAgentService } from './ai-agent.service';
-import { ChatDto, ChatResponseDto } from './dto/chat.dto';
+import { ChatDto } from './dto/chat.dto';
 import { CreateAgentFarmDto, UpdateAgentFarmDto } from './dto/agent-farm.dto';
-import {
-  ApiOperation,
-  ApiResponse,
-  ApiQuery,
-  ApiTags,
-  ApiParam,
-} from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags, ApiParam } from '@nestjs/swagger';
 import { AiDealerAgentService } from './ai-dealer-agent.service';
+import { ChatHistoryService } from './services/chat-history.service';
 
 @ApiTags('AI Agent')
 @Controller('ai')
 export class AiAgentController {
   constructor(
-    private readonly aiAgentService: AiAgentService, 
-    private readonly aiDealerAgentService: AiDealerAgentService) {}
+    private readonly aiAgentService: AiAgentService,
+    private readonly aiDealerAgentService: AiDealerAgentService,
+    private readonly chatHistoryService: ChatHistoryService,
+  ) {}
 
   // @Post('chat')
   // @ApiOperation({ summary: 'Chat with AI agent Kael' })
@@ -111,34 +107,45 @@ export class AiAgentController {
   // }
 
   calculateItemCounts(duration: number) {
-    const durationInHours = duration / 3600; 
+    const durationInHours = duration / 3600;
     const dropCount = Math.floor(durationInHours / 2) * 4; // 4 drops every 2 hours
-    const rarityDistribution = 
+    const rarityDistribution =
       durationInHours <= 2
         ? { common: 70, great: 25, rare: 5, epic: 0 }
         : durationInHours <= 4
-        ? { common: 48, great: 30, rare: 20, epic: 2 }
-        : durationInHours <= 8
-        ? { common: 35, great: 30, rare: 25, epic: 10 }
-        : { common: 20, great: 25, rare: 30, epic: 25 };
+          ? { common: 48, great: 30, rare: 20, epic: 2 }
+          : durationInHours <= 8
+            ? { common: 35, great: 30, rare: 25, epic: 10 }
+            : { common: 20, great: 25, rare: 30, epic: 25 };
 
-    
-    var itemCounts = {
+    const itemCounts = {
       common: 0,
       great: 0,
       rare: 0,
       epic: 0,
     };
-    console.log('Calculating item counts for duration:', durationInHours, 'hours');    
+    console.log(
+      'Calculating item counts for duration:',
+      durationInHours,
+      'hours',
+    );
     console.log('Item counts:', itemCounts);
 
     for (let i = 0; i < dropCount; i++) {
       const randomValue = Math.random() * 100;
       if (randomValue < rarityDistribution.epic) {
         itemCounts.epic++;
-      } else if (randomValue < rarityDistribution.epic + rarityDistribution.rare) {
+      } else if (
+        randomValue <
+        rarityDistribution.epic + rarityDistribution.rare
+      ) {
         itemCounts.rare++;
-      } else if (randomValue < rarityDistribution.epic + rarityDistribution.rare + rarityDistribution.great) {
+      } else if (
+        randomValue <
+        rarityDistribution.epic +
+          rarityDistribution.rare +
+          rarityDistribution.great
+      ) {
         itemCounts.great++;
       } else {
         itemCounts.common++;
@@ -156,7 +163,9 @@ export class AiAgentController {
   })
   async startChatting(@Body() body: ChatDto): Promise<any> {
     try {
-      const result = await this.aiAgentService.initializeFarmerAgent(body.walletAddress);
+      const result = await this.aiAgentService.initializeFarmerAgent(
+        body.walletAddress,
+      );
       console.log(result);
       return result;
     } catch (error) {
@@ -177,7 +186,7 @@ export class AiAgentController {
   async normalChat(@Body() body: ChatDto): Promise<any> {
     try {
       console.log('Running negotiation example...');
-      var result = await this.aiAgentService.handlePlayerMessage(
+      const result = await this.aiAgentService.handlePlayerMessage(
         body.walletAddress,
         body.message,
       );
@@ -220,7 +229,9 @@ export class AiAgentController {
   async startDealing(@Body() body: ChatDto): Promise<any> {
     try {
       console.log('Running negotiation example...');
-      const data = await this.aiDealerAgentService.getAgentFarmData(body.walletAddress);
+      const data = await this.aiDealerAgentService.getAgentFarmData(
+        body.walletAddress,
+      );
       if (!data || !data.isFarming) {
         console.log("Can't find agent farm data or farming not started yet");
         throw new HttpException(
@@ -228,8 +239,11 @@ export class AiAgentController {
           HttpStatus.NOT_FOUND,
         );
       }
-      console.log("Getting agent welcoming message...");
-      const itemCounts = data.itemCounts && data.itemCounts.common ? data.itemCounts : this.calculateItemCounts(data.duration);
+      console.log('Getting agent welcoming message...');
+      const itemCounts =
+        data.itemCounts && data.itemCounts.common
+          ? data.itemCounts
+          : this.calculateItemCounts(data.duration);
       console.log(itemCounts);
       await this.aiDealerAgentService.updateAgentFarmData(body.walletAddress, {
         isFarming: data.isFarming,
@@ -247,7 +261,7 @@ export class AiAgentController {
             rare: 20,
             epic: 30,
           },
-          itemCounts: itemCounts
+          itemCounts: itemCounts,
         },
         {
           minSellRatio: 0.5,
@@ -274,12 +288,14 @@ export class AiAgentController {
   async hagniChat(@Body() body: ChatDto): Promise<any> {
     try {
       console.log('Running negotiation example...');
-      var result = await this.aiDealerAgentService.handlePlayerMessage(
+      const result = await this.aiDealerAgentService.handlePlayerMessage(
         body.walletAddress,
         body.message,
       );
       if (result.outcome && result.outcome === 'accepted') {
-        const data = await this.aiDealerAgentService.getAgentFarmData(body.walletAddress);
+        const data = await this.aiDealerAgentService.getAgentFarmData(
+          body.walletAddress,
+        );
         result.itemCounts = data.itemCounts;
         result.extractedOffer = result.extractedOffer;
       }
@@ -302,10 +318,12 @@ export class AiAgentController {
   async endNegotiation(@Body() body: ChatDto): Promise<any> {
     try {
       console.log('Ending negotiation...');
-      const data = await this.aiDealerAgentService.getAgentFarmData(body.walletAddress);
+      const data = await this.aiDealerAgentService.getAgentFarmData(
+        body.walletAddress,
+      );
       if (!data || !data.isFarming) {
         console.log("Can't find agent farm data or farming not started yet");
-        return { message: 'Agent farm data not found or farming not started'};
+        return { message: 'Agent farm data not found or farming not started' };
       }
       await this.aiDealerAgentService.updateAgentFarmData(body.walletAddress, {
         isFarming: false,
@@ -332,7 +350,9 @@ export class AiAgentController {
   })
   async createAgentFarm(@Body() createAgentFarmDto: CreateAgentFarmDto) {
     try {
-      return await this.aiDealerAgentService.createAgentFarmData(createAgentFarmDto);
+      return await this.aiDealerAgentService.createAgentFarmData(
+        createAgentFarmDto,
+      );
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -350,7 +370,8 @@ export class AiAgentController {
   })
   async getAgentFarm(@Param('walletAddress') walletAddress: string) {
     try {
-      const data = await this.aiDealerAgentService.getAgentFarmData(walletAddress);
+      const data =
+        await this.aiDealerAgentService.getAgentFarmData(walletAddress);
       if (!data) {
         const data = await this.aiDealerAgentService.createAgentFarmData({
           walletAddress: walletAddress,
@@ -412,7 +433,8 @@ export class AiAgentController {
   })
   async deleteAgentFarm(@Param('walletAddress') walletAddress: string) {
     try {
-      const data = await this.aiDealerAgentService.deleteAgentFarmData(walletAddress);
+      const data =
+        await this.aiDealerAgentService.deleteAgentFarmData(walletAddress);
       if (!data) {
         throw new HttpException(
           'Agent farm data not found',
@@ -425,6 +447,28 @@ export class AiAgentController {
         throw error;
       }
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Delete('closeChat/:walletAddress')
+  @ApiOperation({ summary: 'Delete chat history for a user' })
+  @ApiParam({
+    name: 'walletAddress',
+    description: 'Wallet address of the user',
+  })
+  async deleteChatHistory(
+    @Param('walletAddress') walletAddress: string,
+  ): Promise<any> {
+    try {
+      const result =
+        await this.chatHistoryService.clearChatHistory(walletAddress);
+      return result;
+    } catch (error) {
+      console.error('Error deleting chat history:', error);
+      throw new HttpException(
+        'An error occurred while processing your request',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
